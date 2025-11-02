2a6b9057db8373f499e4218bf753044a642dbe6e
Adam Bien <abien@adam-bien.com>
2025-11-02 15:22:21 +0100

    code refactored / agent ARN configured

--- agent-st/src/test/java/airhacks/agentcore/greetings/boundary/GreetingAgentIT.java
+++ agent-st/src/test/java/airhacks/agentcore/greetings/boundary/GreetingAgentIT.java
@@ -1,39 +1,48 @@
 package airhacks.agentcore.greetings.boundary;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.bedrockagentcore.BedrockAgentCoreClient;
+import software.amazon.awssdk.services.bedrockagentcore.model.InvokeAgentRuntimeRequest;
 
 @QuarkusTest
 public class GreetingAgentIT {
 
-    @Inject
-    @ConfigProperty(name = "aws.region",defaultValue = "eu-central-1")
-    String region;
 
     @Inject
-    @ConfigProperty(name = "aws.account.id")
-    String accountId;
-
-    /**
-     * The agent ID as configured in the CDK stack
-     */
-    String agentId = "QuarkusAgentRuntime";
-
-
-   String arn() {
-        return String.format(
-            "arn:aws:bedrock-agentcore:%s:%s:agent/%s",
-            this.region, this.accountId, this.agentId
-        );
-    } 
-
+    @ConfigProperty(name = "agent.arn")
+    String agentArn;
+        
+    
     @Test
-    void invokeAgent() {
+    void invokeAgent() throws IOException {
+        var client = BedrockAgentCoreClient.create();        
+        var message = """
+                    {
+                        "message": "hello, duke",
+                        "context": {}
+                    }""";
 
+        var request = InvokeAgentRuntimeRequest.builder()
+                .agentRuntimeArn(this.agentArn)
+                .payload(SdkBytes.fromUtf8String(message))
+                .contentType("application/json")
+                .build();
+
+        var response = client.invokeAgentRuntime(request);
+        var content = new String(response.readAllBytes());
+        assertThat(content).isNotEmpty();
+        System.out.println(content);
+        var runtimeSessionId = response.response().runtimeSessionId();
+        assertThat(runtimeSessionId).isNotEmpty();
     }
-
 }
