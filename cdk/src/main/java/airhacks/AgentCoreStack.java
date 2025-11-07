4bf600fe4a362869dc6a273f5e4d2e38cb9d8c45
Adam Bien <abien@adam-bien.com>
2025-11-07 19:32:15 +0100

    fqn import removed

--- cdk/src/main/java/airhacks/AgentCoreStack.java
+++ cdk/src/main/java/airhacks/AgentCoreStack.java
@@ -5,16 +5,15 @@ import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.bedrock.agentcore.alpha.AgentRuntimeArtifact;
 import software.amazon.awscdk.services.bedrock.agentcore.alpha.RuntimeNetworkConfiguration;
+import software.amazon.awscdk.services.bedrock.agentcore.alpha.Runtime;
 import software.constructs.Construct;
 
-public class AgentCoreStack
-        extends Stack {
+public class AgentCoreStack extends Stack {
             
-
     public AgentCoreStack(Construct scope, String appName, StackProps stackProps) {
         super(scope, ConventionalDefaults.stackName(appName, "agent-core"), stackProps);
         var agentArtifact = AgentRuntimeArtifact.fromAsset(ConventionalDefaults.dockerFilePath.toString());
-        var runtime = software.amazon.awscdk.services.bedrock.agentcore.alpha.Runtime.Builder.create(this, "AgentRuntime")
+        var runtime = Runtime.Builder.create(this, "AgentRuntime")
                 .runtimeName("QuarkusAgentRuntime")
                 .agentRuntimeArtifact(agentArtifact)
                 .networkConfiguration(RuntimeNetworkConfiguration.usingPublicNetwork())
